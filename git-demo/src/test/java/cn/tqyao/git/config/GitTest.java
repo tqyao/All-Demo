027fee2e45c68dfe73b5484b6cdc71f2a05e0797
谭钦瑶 <790461387@qq.com>
2020-10-21 11:35:24 +0800

    git commit two

---
+++ git-demo/src/test/java/cn/tqyao/git/config/GitTest.java
@@ -0,0 +1,15 @@
+/**
+ * Copyright 2020-2030 Jinhui Technology Co. LTD  All Rights Reserved.
+ */
+package cn.tqyao.git.config;
+
+/**
+ *
+ * .<br>
+ * @author tanqinyao<br>
+ * @date Create in 2020/10/21 11:02 <br>
+ * @version  <br>
+ * @description  <br>
+ */
+public class GitTest {
+}
