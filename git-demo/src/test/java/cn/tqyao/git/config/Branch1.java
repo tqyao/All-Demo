467f464be3fad58bf86d1bacfe677f3f8b0d933e
谭钦瑶 <790461387@qq.com>
2020-10-22 10:26:07 +0800

    添加注释

---
+++ git-demo/src/test/java/cn/tqyao/git/config/Branch1.java
@@ -0,0 +1,19 @@
+/**
+ * Copyright 2020-2030 Jinhui Technology Co. LTD  All Rights Reserved.
+ */
+package cn.tqyao.git.config;
+
+/**
+ *
+ * .<br>
+ * @author tanqinyao<br>
+ * @date Create in 2020/10/21 19:46 <br>
+ * @version  <br>
+ * @description  <br>
+ */
+public class Branch1 {
+    public static void main(String[] args) {
+        //添加注释
+        System.out.println("--");
+    }
+}
